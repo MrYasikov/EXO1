31d5f8e70509f104a025cdb5e9f777e0cdfc046a
Bastien <bastienmwamba2@gmail.com>
2026-06-17 18:41:51 +0100

    ajout des fonctionnalités

--- index.py
+++ index.py
@@ -1,9 +1,16 @@
 import random
 num = random.randint(1, 100)
 print("Bienvenu dans le jeu de devinettes!")#affiche un message de bienvenue pour le jeu de devinetttes 
-turn = 3
+turn = 3 
 while True:
-    guess = int(input("Entrez votre nombre deviné (entre 1 et 100): "))
+    try:
+        guess = int(input("Entrez votre nombre deviné (entre 1 et 100): "))
+    except ValueError:
+        print("Veuillez entrer un nombre valide.")#affiche un message pour indiquer que l'entrée n'est pas un nombre valide
+        continue
+    if guess < 1 or guess > 100:
+        print("Veuillez entrer un nombre entre 1 et 100.")#affiche un message pour indiquer que le nombre deviné n'est pas dans la plage valide
+        continue
     if guess == num:
         print("Très bien! Vous avez deviné le nombre {num} en {turn} coups!")#affiche un message pour indiquer que le joueur a deviné le nombre
         break
