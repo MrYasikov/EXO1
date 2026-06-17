b4dcbf9a830a0ce8772255f3f6ea1fd225aff1dd
Bastien <bastienmwamba2@gmail.com>
2026-06-17 19:52:51 +0100

    amélioration

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ print("Bienvenu dans le jeu de devinettes!")#affiche un message de bienvenue pou
 turn = 3 
 while True:
     try:
-        guess = int(input("Entrez votre nombre deviné (entre 1 et 100): "))
+        guess = int(input(f"(Il vous reste {turn} coup(s))Entrez votre nombre deviné (entre 1 et 100): "))
     except ValueError:
         print("Veuillez entrer un nombre valide.")#affiche un message pour indiquer que l'entrée n'est pas un nombre valide
         continue
@@ -12,12 +12,12 @@ while True:
         print("Veuillez entrer un nombre entre 1 et 100.")#affiche un message pour indiquer que le nombre deviné n'est pas dans la plage valide
         continue
     if guess == num:
-        print("Très bien! Vous avez deviné le nombre {num} en {turn} coups!")#affiche un message pour indiquer que le joueur a deviné le nombre
+        print(f"Très bien! Vous avez deviné le nombre {num} en {turn} coups!")#affiche un message pour indiquer que le joueur a deviné le nombre
         break
     elif guess < num:
-        print("Too low! Try again.")#affiche un message pour indiquer que le nombre deviné est trop bas
+        print("trop petit! reessayez.")#affiche un message pour indiquer que le nombre deviné est trop bas
     else:
-        print("Too high! Try again.")#affiche un message pour indiquer que le nombre deviné est trop haut
+        print("trop grand! reessayez.")#affiche un message pour indiquer que le nombre deviné est trop haut
     turn -= 1
     if turn == 0:
         print(f"Game over! The number was {num}.")#affiche un message de fin de jeu si le joueur n'a plus de coups restants et révèle le nombre à deviner 
